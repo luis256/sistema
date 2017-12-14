1d7dc58861e6913f5747fd4a05b3da1ce620beaf
luis2308 <32052616+luis2308@users.noreply.github.com>
2017-12-13 20:38:45 -0600

    sistem

---
+++ database/medisys.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-12-2017 a las 22:53:45
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `medisys`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicamentos`
+--
+
+CREATE TABLE `medicamentos` (
+  `codigo` varchar(7) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `precio_compra` int(11) NOT NULL,
+  `precio_venta` int(11) NOT NULL,
+  `unidad` varchar(20) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `created_user` int(3) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_user` int(3) NOT NULL,
+  `updated_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `medicamentos`
+--
+
+INSERT INTO `medicamentos` (`codigo`, `nombre`, `precio_compra`, `precio_venta`, `unidad`, `stock`, `created_user`, `created_date`, `updated_user`, `updated_date`) VALUES
+('B000001', 'aceite oliva', 56, 150, 'cajas', 50, 1, '2017-11-13 21:09:03', 1, '2017-11-13 21:09:46'),
+('B000002', 'vainilla', 45, 110, 'caja', 65, 1, '2017-11-13 21:23:32', 1, '2017-11-13 21:46:04'),
+('B000003', 'aceitunas', 67, 150, 'botellas', 54, 1, '2017-11-13 21:24:27', 1, '2017-11-13 22:45:49'),
+('B000004', 'vainilla 500ml', 220, 300, 'cajas', 0, 1, '2017-11-15 22:13:43', 1, '2017-11-15 22:13:43');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transaccion_medicamentos`
+--
+
+CREATE TABLE `transaccion_medicamentos` (
+  `codigo_transaccion` varchar(15) NOT NULL,
+  `fecha` date NOT NULL,
+  `codigo` varchar(7) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `created_user` int(3) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tipo_transaccion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `transaccion_medicamentos`
+--
+
+INSERT INTO `transaccion_medicamentos` (`codigo_transaccion`, `fecha`, `codigo`, `numero`, `created_user`, `created_date`, `tipo_transaccion`) VALUES
+('TM-2017-0000001', '2017-11-13', 'B000001', 50, 1, '2017-11-13 21:09:45', 'Entrada'),
+('TM-2017-0000002', '2017-11-13', 'B000003', 70, 1, '2017-11-13 21:45:41', 'Entrada'),
+('TM-2017-0000003', '2017-11-13', 'B000002', 65, 1, '2017-11-13 21:46:04', 'Entrada'),
+('TM-2017-0000004', '2017-11-13', 'B000003', 16, 1, '2017-11-13 22:45:49', 'Salida');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_user` int(3) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `name_user` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `telefono` varchar(13) DEFAULT NULL,
+  `foto` varchar(100) DEFAULT NULL,
+  `permisos_acceso` enum('Super Admin','Gerente','Almacen') NOT NULL,
+  `status` enum('activo','bloqueado') NOT NULL DEFAULT 'activo',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_user`, `username`, `name_user`, `password`, `email`, `telefono`, `foto`, `permisos_acceso`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'San Salvador', '21232f297a57a5a743894a0e4a801fc3', 'info@sist.com', '7025', 'user-default.png', 'Super Admin', 'activo', '2017-04-01 08:15:15', '2017-11-13 18:11:07');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `medicamentos`
+--
+ALTER TABLE `medicamentos`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `created_user` (`created_user`),
+  ADD KEY `updated_user` (`updated_user`);
+
+--
+-- Indices de la tabla `transaccion_medicamentos`
+--
+ALTER TABLE `transaccion_medicamentos`
+  ADD PRIMARY KEY (`codigo_transaccion`),
+  ADD KEY `id_barang` (`codigo`),
+  ADD KEY `created_user` (`created_user`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `level` (`permisos_acceso`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `medicamentos`
+--
+ALTER TABLE `medicamentos`
+  ADD CONSTRAINT `medicamentos_ibfk_1` FOREIGN KEY (`created_user`) REFERENCES `usuarios` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `medicamentos_ibfk_2` FOREIGN KEY (`updated_user`) REFERENCES `usuarios` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `transaccion_medicamentos`
+--
+ALTER TABLE `transaccion_medicamentos`
+  ADD CONSTRAINT `transaccion_medicamentos_ibfk_1` FOREIGN KEY (`codigo`) REFERENCES `medicamentos` (`codigo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaccion_medicamentos_ibfk_2` FOREIGN KEY (`created_user`) REFERENCES `usuarios` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
